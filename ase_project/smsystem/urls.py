2966f2d5b78c8f955ecd22f0376deb527c6a36f4
Gowthukatla Venkata Charan <30764040+gvenkatacharan@users.noreply.github.com>
2018-11-01 15:59:20 +0530

    Add files via upload

--- ase_project/smsystem/urls.py
+++ ase_project/smsystem/urls.py
@@ -1,10 +1,42 @@
 from django.conf.urls import url,include
 from . import views
 urlpatterns = [
+    url(r'^$',views.signin,name="login"),
+    url(r'^$',views.signout,name="signout"),
     url(r'^studenthome/$',views.studenthome,name="studenthome"),
     url(r'^studentcomplaints/$',views.studentcomplaints,name="studentcomplaints"),
     url(r'^studentmatchresults/$',views.studentmatchresults,name="studentmatchresults"),
     url(r'^studentschedules/$',views.studentschedules,name="studentschedules"),
     url(r'^studentsettings/$',views.studentsettings,name="studentsettings"),
-    url(r'^sendannouncement/$',views.sendannouncement,name="sendannouncement"),
+
+    
+
+    url(r'^adminhome/$',views.adminhome,name="adminhome"),
+    url(r'^adminschedules/$',views.adminschedules,name="adminschedules"),
+    url(r'^admincoaches/$',views.admincoaches,name="admincoaches"),
+    url(r'^adminaddcoaches/$',views.adminaddcoaches,name="adminaddcoaches"),
+    url(r'^adminaddschedules/$',views.adminaddschedules,name="adminaddschedules"),
+    url(r'^admingames/$',views.admingames,name="admingames"),
+    url(r'^adminaddgames/$',views.adminaddgames,name="adminaddgames"),
+    url(r'^adminsettings/$',views.adminsettings,name="adminsettings"),
+    url(r'^adminperformance/$',views.adminperformance,name="adminperformance"),
+    url(r'^adminviewperformance/$',views.adminviewperformance,name="adminviewperformance"),
+    url(r'^admincomplaints/$',views.admincomplaints,name="admincomplaints"),
+
+    url(r'^coachhome/$',views.coachhome,name="coachhome"),
+    url(r'^coachschedules/$',views.coachschedules,name="coachschedules"),
+    url(r'^coachaddschedules/$',views.coachaddschedules,name="coachaddschedules"),
+    url(r'^coachsettings/$',views.coachsettings,name="coachsettings"),
+    url(r'^coachupdatesettings/$',views.coachupdatesettings,name="coachupdatesettings"),
+    url(r'^coachperformance/$',views.coachperformance,name="coachperformance"),
+    url(r'^coachmatchresults/$',views.coachmatchresults,name="coachmatchresults"),
+    url(r'^coachviewperformance/$',views.coachviewperformance,name="coachviewperformance"),
+    url(r'^coachaddmatchresults/$',views.coachaddmatchresults,name="coachaddmatchresults"),
+    url(r'^coachaddperformance/$',views.coachaddperformance,name="coachaddperformance"),
+    url(r'^coachupdateperformance/$',views.coachupdateperformance,name="coachupdateperformance"),
+    url(r'^coachcomplaints/$',views.coachcomplaints,name="coachcomplaints"),
+    
+
+
+    
 ]
